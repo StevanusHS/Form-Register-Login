ccf8c6d221c5111efc07907b66d39650553e0940
ahlanz <ahlangaffar@gmail.com>
2022-07-26 19:09:08 +0700

    navBar-done

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <!-- navbar -->
+    <header>
+        <nav class="navbar navbar-expand-lg ">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="./img/logowarming.png" alt="logo" width="auto" height="50px"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link text-light" href="#">About</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link text-light" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link text-light" href="#">Service</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- end navbar -->
+    <main></main>
+    <footer></footer>
+    <script src="./js/bootstrap.js"></script>
+    <script src="./js/popper.min.js"></script>
+</body>
+
+</html>
